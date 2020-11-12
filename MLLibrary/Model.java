94da758b8b907868a01443533e5ab54172d69b0e
dionzyrene <72721440+dionzyrene@users.noreply.github.com>
2020-11-11 23:23:46 -0800

    KNNModel Added

--- MLLibrary/Model.java
+++ MLLibrary/Model.java
@@ -1,21 +1,36 @@
 import java.util.ArrayList;
 
 public abstract class Model{
-	private double maxRedX=0;
-	private double maxRedY=0;
+	private double maxf1=0;
+	private double maxf2=0;
+
 
 	
 	
-		abstract void train (ArrayList<DataPoint>data) {
-			double maxRedX =0;
-			double maxRedX=0;
+		abstract void train (DataPoint[] sampleData) {
+			double maxRedf1 =0;
+			double maxRedf2=0;
+			int numf1 =0;
+			int numf2=0;
 		
+			for (int i=0; i<trainData.length; i++) {
+				if (trainData[i].getLabel()== "red") {
+					maxf1 += trainData[i].getf1();
+					numf1+= 1;
+				}
+				
+				if (trainData[i].getLabel()== "blue") {
+					maxf2 += trainData[i].getf1();
+					numf2 +=1;
+						
+				}
+			}
 		}
 		abstract String test (ArrayList<DataPoint> data) {
 			DataPoint testData = data [0];
 			//dummy model
 			
-			if (Math.abs(testDate.getX()- this.maxRedX)> Math.abs(testDate.getX()- this.maxBlueX)) {
+			if (Math.abs(testDate.getX()- this.maxf1)> Math.abs(testDate.getX()- this.maxf2)) {
 				return "blue";
 			
 		}
@@ -25,6 +40,7 @@ public abstract class Model{
 			
 		}
 		abstract Double getAccuracy(ArrayList<DataPoint> data) {
+			
 		
 		}
 		abstract Double getPrecision(ArrayList<DataPoint> data) {
@@ -32,4 +48,3 @@ public abstract class Model{
 	
 }
 }
-
